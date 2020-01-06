0e34315d3f66bd440d7ec241964ee387912b5ec7
David Kalina <davidkalina1999@gmail.com>
2020-01-06 09:44:41 -0700

    Greatly refactored code

--- public/todo.js
+++ public/todo.js
@@ -3,6 +3,8 @@
 
 $(() => {
     const $addTodoButton = $('.addTodo');
+    const $complete = $('.complete');
+    const $todo = $('.todo');
     const $toggle = $('.toggle');
     const $description = $('.description');
     const text = $description.text();
@@ -10,39 +12,32 @@ $(() => {
 
     let path = window.location.pathname; 
     let user = path.slice(8, path.length);
-    let date = new Date();
 
-    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    $addTodoButton.on('click', (e) =>{
+    
+    const createTodoForm = () => {
         const $todo = $(`
         <form class ="todo-creator" action="" method="POST">
-            <input type="text" name="description" placeholder="Description">
-            <input type="text" name="dateCreated" value="${months[new Date().getMonth()]} ${new Date().getDate()}" readonly>
-            <input class="hidden" type="text" name="user" value="${user}">
-            <input class="hidden" type="text" name="completed" value="false">
-                    <div class="todoHandler">
-                        <button type="submit">Add</button>
-                        <button class="cancel" type="button">Cancel</button>
-                    </div>
-        </form>
-        `);
-        // console.log($todoDiv);
+        <input type="text" name="description" placeholder="Description">
+        <input type="text" name="dateCreated" value="${months[new Date().getMonth()]} ${new Date().getDate()}" readonly>
+        <input class="hidden" type="text" name="user" value="${user}">
+        <input class="hidden" type="text" name="completed" value="false">
+        <div class="todoHandler">
+        <button type="submit">Add</button>
+        <button class="cancel" type="button">Cancel</button>
+        </div>
+        </form>`);
         $todoDiv.append($todo);
-        
-        $('.cancel').on('click', (e) => {
-            console.log($(e.target).parent().parent().remove());
-        });
-    });
+        $('.cancel').click((e) => {console.log($(e.target).parent().parent().remove())});
+    }
 
+    const completeTodo = () => {
+        $todo.attr('action', '?_method=DELETE');
+        $complete.attr('value', true);
+    }
 
-    $('.complete').on('click', (e) => {
-        $('.todo').attr('action', '?_method=DELETE');
-        $('.complete').attr('value', true);
-    });
-
-    $('.toggle').on('click', (e) => {
+    const editTodo = (e) => {
         $(e.target).toggleClass('save');
         
         let localDescription = $(e.target).parent().parent().parent().find('.description');
@@ -59,27 +54,17 @@ $(() => {
                 $(e.target).attr('type', 'submit');
             });
         }
-        
-        // $('.save').on('click', (e) => {
-        //     alert('Hello');
-        //     $('.save').attr('type', 'submit');
-        // });
-    });
+    };
+    
+    $addTodoButton.click(createTodoForm);
+    $complete.click(completeTodo);
+    $toggle.on('click', (e) => {editTodo(e)});
+});
     
 
 
-    // $('.toggle').on('click', (e) => {
-    //     if($(e.target).text() === "Save") {
-    //         $(e.target).text('Edit');
-    //     } else {
-    //         $(e.target).text('Save');
-    //         $(e.target).attr('type', 'submit');
-    //         let localDescription = $(e.target).parent().parent().parent().find('.description');
-    //         localDescription.attr('readonly', false);
-    //         $('.save').attr('type', 'submit');
-    //     }
-    // });
-});
+    
+
 
 
 
