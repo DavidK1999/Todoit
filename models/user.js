ba1b5a756d244f5c5496a8d020e4b8438e6e7e52
David Kalina <davidkalina1999@gmail.com>
2020-01-07 08:59:22 -0700

    Fixed date

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: String,
     password: {type: String, required: true},
+    settings: String
 });
 
 const User = mongoose.model('User', userSchema);
